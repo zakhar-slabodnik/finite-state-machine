3b72a09f8bafa66df5b71b923c3ebdf0eadb5142
Zakhar Slabodnik <zakhar.slabodnik@gmail.com>
2017-02-05 23:27:56 +0300

    Implemented specs

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,58 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+      if (config === undefined) {
+        throw new Error();
+      }
+      this.config = config;
+      this.state = config.initial;
+      this.history = [config.initial];
+      this.cursor = 0;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+      return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+      if (this.config.states[state] === undefined) {
+        throw new Error();
+      }
+      this.history = this.history.slice(0, this.cursor + 1);
+      this.history.push(this.state);
+      this.cursor += 1;
+      this.state = state;
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+      if (this.config.states[this.state].transitions[event] === undefined) {
+        throw new Error();
+      }
+      this.state = this.config.states[this.state].transitions[event];
+      this.history = this.history.slice(0, this.cursor + 1);
+      this.history.push(this.state);
+      this.cursor += 1; // history: [ normal state ], cursor: 1 -> [normal, state1, state2], cursor: 2
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+      return this.state = 'normal';
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
